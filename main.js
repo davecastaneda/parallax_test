58df15d6c86fd36bce96ee5d48ca279248b8bb49
davecastaneda <smopo24@gmail.com>
2018-08-08 00:03:51 -0500

    Added last two divs

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const parallax = document.getElementById("parallax");
+const parallaxTwo = document.getElementById("bkdsparallax");
+
 
 window.addEventListener("scroll", function() {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
+
+window.addEventListener("scroll", function() {
+    let offset = window.pageYOffset;
+    parallaxTwo.style.backgroundPositionY = offset * 1.5 + "px";
 })
